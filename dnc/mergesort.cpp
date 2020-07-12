c43c1c1a17a7414a67219c49a63ffc72dbf128ca
gearuprepo <sb.desikan@gmail.com>
2020-07-12 20:25:49 +0530

    Fixes

--- dnc/mergesort.cpp
+++ dnc/mergesort.cpp
@@ -18,7 +18,7 @@ bool test(){
     //int arr[5] = {10, 1, 8, 2, };
     int arr[8] = {10,1,7,2,11,15,66,32};
     int len = sizeof(arr)/sizeof(arr[0]);    //int - 4bits, sizeof(arr) = 4B * 5 = 20 bit
-    mergesort(arr,0,len);
+    mergesort(arr,0,len-1);
     return true;
 }
     /*eg->[10,1,7,6]
@@ -32,14 +32,14 @@ void mergesort(int* inp,int start,int end){
     //left recursion
     //right recursion
     //merge
-    if ((end-start) <=0){//if array has no elements, just return
+    if ((end-start) <1){//if array has no elements, just return
     //Return
     }else if((end-start) == 1){// Two elements, needs compare and swap
-        swap(&inp[0],&inp[1]);
+        swap(&inp[start],&inp[end]);
     }else{
         int mid = ( start + end )/2;
-        mergesort(inp,start,mid-1); //Left recursion
-        mergesort(inp,mid,end-1); //Right recursion
+        mergesort(inp,start,mid); //Left recursion
+        mergesort(inp,mid+1,end); //Right recursion
         merge(inp,start,mid, end);
     }
     std::cout<<"hello";
@@ -50,16 +50,25 @@ void merge(int* inp,int start,int mid,int end){
     //mid+1 to end is sorted
     int lctr = start;
     int rctr = mid+1;
+    int ctr=0;
     //left 1,10, right 6,7
-
-    while (lctr < mid || rctr< end-1){
-        if(inp[lctr]>inp[rctr]){
-            swap(&inp[lctr],&inp[rctr]);
-            lctr++;
+    int buff[end+1] = {};
+    while (ctr<=end){
+        if(lctr==mid){
+            buff[ctr] = inp[rctr];
             rctr++;
-        }else{
+        }else if(rctr == end){
+            buff[ctr] = inp[lctr];
             lctr++;
         }
+        else if(inp[lctr]>inp[rctr]){
+            buff[ctr] = inp[rctr];
+            rctr++;
+        }else{
+            buff[ctr] = inp[lctr];
+            lctr++;
+        }
+        ctr++;
     }
     std::cout<<"End";
 }
